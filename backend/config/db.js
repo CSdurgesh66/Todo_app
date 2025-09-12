49a1f1cec8499d84f7f8d6c4a605372c6cea37f6
Durgesh Kumar <durgesh42416@gmail.com>
2025-09-12 22:39:06 +0530

    Debug: MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-const DB_URl = process.env.DB_URl;
+const DB_URL = process.env.DB_URL;
 
 exports.connectDB = async() => {
     try{
-        await mongoose.connect(DB_URl);
+        await mongoose.connect(DB_URL);
         console.log("Database connected successfully");
 
     }catch(error){
